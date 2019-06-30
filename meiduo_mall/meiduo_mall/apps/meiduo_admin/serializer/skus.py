d3c1fff27f2c401db3c34bc90150134e1dc932ef
ldy_com <572314705@qq.com>
2019-06-30 15:45:05 +0800

    保存sku表数据内容

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializer/skus.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializer/skus.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from goods.models import SKU, SKUSpecification, GoodsCategory, SpecificationOption, SPUSpecification
@@ -44,9 +45,9 @@ class SKUSerializer(serializers.ModelSerializer):
         """
     # 关联嵌套序列化返回
     spu = serializers.StringRelatedField(read_only=True)
-    spu_id = serializers.IntegerField(read_only=True)
-    caption = serializers.StringRelatedField(read_only=True)
-    category_id = serializers.IntegerField(read_only=True)
+    spu_id = serializers.IntegerField()
+    category = serializers.StringRelatedField(read_only=True)
+    category_id = serializers.IntegerField()
 
     # 返回关联的sku具体规格表数据
     specs = SKUSpecificationSeriazier(many=True)
@@ -54,6 +55,34 @@ class SKUSerializer(serializers.ModelSerializer):
     class Meta:
         model = SKU
         fields = '__all__'
+
+    # 重写保存方法
+
+    # @transaction.atomic()
+    def create(self, validated_data):
+        # 使用事务
+        with transaction.atomic():
+            # 设置保存
+            save_point = transaction.savepoint()
+            try:
+                # 1、保存sku表
+                specs = validated_data['specs']
+                del validated_data['specs']
+                sku = SKU.objects.create(**validated_data)
+                # 2、sku具体规格表
+                for spec in specs:
+                    SKUSpecification.objects.create(sku=sku, spec_id=spec['spec_id'], option_id=spec['option_id'])
+
+
+            except Exception as e:
+                print(e)
+                # 捕获异常回滚到保存点
+                transaction.savepoint_rollback(save_point)
+                raise serializers.ValidationError('保存数据失败')
+            else:
+                # 事务提交
+                transaction.savepoint_commit(save_point)
+                return sku
 
 
 class GoodsCategorySerializer(serializers.ModelSerializer):
@@ -77,6 +106,7 @@ class OptionSerialzier(serializers.ModelSerializer):
         # 指定那些字段生成
         fields = ('id', 'value')
 
+
 class SPUSpecificationSerialzier(serializers.ModelSerializer):
     """
             SPU规格表
@@ -95,8 +125,8 @@ class SPUSpecificationSerialzier(serializers.ModelSerializer):
     spu_id = serializers.IntegerField()
     # 关联的规格选项表
     options = OptionSerialzier(many=True)
-    class Meta:
 
+    class Meta:
         # 指定根据那个模型类生成序列化器字段
         model = SPUSpecification
         # 指定那些字段生成
